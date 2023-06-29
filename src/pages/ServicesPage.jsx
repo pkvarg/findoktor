2fa118292967296a0923aad78b6872992a725daf
peter varga <pkvarg@yahoo.se>
2023-06-29 16:56:01 +0200

    mapbox ? eslint npm ?

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Map, { GeolocateControl } from 'react-map-gl'
 
 const ServicesPage = () => {
-  return <div>ServicesPage</div>
+  const token = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN
+
+  const [viewPort, setViewPort] = useState({
+    latitude: 48.16096133288094,
+    longitude: 17.164729724275535,
+    zoom: 17,
+  })
+
+  return (
+    <>
+      <Map
+        mapboxAccessToken={token}
+        initialViewState={{
+          ...viewPort,
+        }}
+        style={{ width: 600, height: 400 }}
+        mapStyle='mapbox://styles/mapbox/streets-v12'
+      >
+        <GeolocateControl
+          positionOptions={{ enableHighAccuracy: true }}
+          trackUserLocation={true}
+        />
+      </Map>
+    </>
+  )
 }
 
 export default ServicesPage
